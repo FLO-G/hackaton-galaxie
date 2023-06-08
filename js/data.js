d330bae0ec81f051e2873dec195a96d2c4a3008c
MSO <mathieu.soussignan@hotmail.fr>
2023-06-08 17:05:26 +0200

    [Improved] calcul de la difference

--- js/data.js
+++ js/data.js
@@ -1,38 +1,56 @@
-// Charger les données des fichiers JSON A et B
-const jsonA = '[{"redshift": 0.8182060549, "valeur": 10}, {"redshift": 1.383214796, "valeur": 20}]';
-const jsonB = '[{"redshift": 0.8182060549, "valeur": 12}, {"redshift": 1.383214796, "valeur": 18}]';
-
-const objetsA = JSON.parse(jsonA);
-const objetsB = JSON.parse(jsonB);
-
-// Tableau pour stocker les différences
-const differences = [];
-
-// Parcourir les objets du fichier JSON A
-for (const objetA of objetsA) {
-  const redshift = objetA.redshift;
-
-  // Rechercher l'objet correspondant dans le fichier JSON B
-  const objetB = objetsB.find((objet) => objet.redshift === redshift);
-
-  if (objetB) {
-    // Effectuer la comparaison et calculer les différences
-    const difference = {
-      redshift: redshift,
-        difference: Math.abs((objetA.valeur - objetB.valeur)) / objetB.valeur
-    };
-
-    // Stocker la différence dans le tableau
-    differences.push(difference);
-  }
+// Fonction pour charger les données JSON à partir d'une URL
+async function chargerJSON(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-// Générer un nouveau JSON à partir du tableau de différences
-const nouveauJSON = JSON.stringify(differences);
+// Charger les données du fichier JSON A
+const urlJSONA = './data/221410_A.json';
+const donneesA = chargerJSON(urlJSONA);
 
-// Sélectionner la div par son ID
-const resultatDiv = document.getElementById('resultat');
+// Charger les données du fichier JSON B
+const urlJSONB = './data/221410_B.json';
+const donneesB = chargerJSON(urlJSONB);
 
-// Mettre à jour le contenu de la div avec le nouveau JSON
-resultatDiv.textContent = nouveauJSON;
-// resultatDiv.textContent = nouveauJSON;
+// Utiliser les données récupérées
+Promise.all([donneesA, donneesB])
+  .then(([dataA, dataB]) => {
+    // Effectuer les comparaisons et générer le nouveau JSON
+      const differences = [];
+
+    // Parcourir les objets du fichier JSON A
+    for (const objetA of dataA) {
+        const redshift = objetA["Redshift"];
+        console.log(objetA);
+
+      // Rechercher l'objet correspondant dans le fichier JSON B
+      const objetB = dataB.find((objet) => objet["Redshift"] === redshift);
+        console.log(objetB);
+        if (objetB) {
+            let difference;
+            objetA.forEach(element => {
+                // Effectuer la comparaison et calculer les différences
+                difference = {
+                  redshift: redshift,
+                  difference: Math.abs((element - objetB[element])) / objetB[element]
+                };
+        
+                // Stocker la différence dans le tableau
+                differences.push(difference); 
+            });
+
+        }
+    }
+    // Générer un nouveau JSON à partir du tableau de différences
+    const nouveauJSON = JSON.stringify(differences);
+
+    // Sélectionner la div par son ID
+    const resultatDiv = document.getElementById('resultat');
+
+    // Mettre à jour le contenu de la div avec le nouveau JSON
+    resultatDiv.textContent = nouveauJSON;
+  })
+  .catch(error => {
+    console.error('Une erreur s\'est produite lors du chargement des données JSON :', error);
+  });
