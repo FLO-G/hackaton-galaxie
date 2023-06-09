03ff31d79d455a05f4d6b121bda56de211366fec
rbufnoir <remi.bufnoir@free.fr>
2023-06-09 11:56:25 +0200

    ainsi va la vie

--- js/data.js
+++ js/data.js
@@ -35,36 +35,48 @@ Promise.all([dataA, dataB])
 		console.error('Une erreur s\'est produite lors du chargement des données JSON :', error);
 	});
 
-function initSelector(array) {
+	function initSelector(array) {
 	let selector = document.getElementById('selector');
-
+	
 	for (let i = 0; i < array.length; i++) {
-			let option = document.createElement('option');
-			option.innerHTML = `${i}: ${array[i]["Redshift"][0]}`;
-			option.setAttribute('id', "rank"+i);
-			selector.appendChild(option);
+		let option = document.createElement('option');
+		option.innerHTML = `${array[i]["Rank"][0]}: ${array[i]["Redshift"][0]}`;
+		option.setAttribute('id', "rank" + i);
+		selector.appendChild(option);
 	}
-
+	
 	selector.addEventListener('change', () => {
 		let index = selector.selectedIndex;
-
-		displayArray(index, array);
+		
+		displayArray(index - 1, array);
+		parseTable();
 	});
 }
 
 function displayArray(index, array) {
 	let table = document.getElementById('table');
 	table.children[1].innerText = "";
-		for (key in array[index]) {
-			let tr = document.createElement('tr');
+	for (key in array[index]) {
+		let tr = document.createElement('tr');
+		let td = document.createElement('td');
+		td.innerHTML = key;
+		tr.appendChild(td);
+		for (let j = 0; j < array[index][key].length; j++) {
 			let td = document.createElement('td');
-			td.innerHTML = key;
+			td.innerHTML = array[index][key][j];
 			tr.appendChild(td);
-			for (let j = 0; j < array[index][key].length; j++) {
-				let td = document.createElement('td');
-				td.innerHTML = array[index][key][j];
-				tr.appendChild(td);
-			}
-			table.children[1].appendChild(tr);
 		}
+		table.children[1].appendChild(tr);
+	}
+}
+
+function parseTable() {
+	let tbody = document.getElementById('table').children[1];
+
+	for(let i = 0; i < tbody.childElementCount; i++) {
+		if (tbody.children[i].lastElementChild.innerHTML == "0" || tbody.children[i].lastElementChild.innerHTML == "NaN") {
+			tbody.children[i].remove();
+			i--;
+		}
+	}
 }
