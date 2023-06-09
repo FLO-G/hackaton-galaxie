67329a7b464f3318c4d79ea7c5f022c8bca14f2c
rbufnoir <remi.bufnoir@free.fr>
2023-06-09 10:33:30 +0200

    more array

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@ const dataA = loadJSON(urlJSONA);
 const dataB = loadJSON(urlJSONB);
 
 Promise.all([dataA, dataB])
-.then(([arrayA, arrayB]) => {
+	.then(([arrayA, arrayB]) => {
 		const arrayT = [];
 
 		for (const objectA of arrayA) {
@@ -23,13 +23,28 @@ Promise.all([dataA, dataB])
 						treshold = objectA[key];
 					else
 						treshold = (Math.round(Math.abs(objectA[key] - objectB[key]) / objectB[key] * 100 * 100) / 100);
-					
+
 					result[key] = [objectA[key], objectB[key], treshold];
 				}
 				arrayT.push(result);
 			}
 		}
-		console.log(arrayT)
+		console.log(typeof arrayT);
+		let table = document.getElementById('table');
+		for (let i = 0; i < arrayT.length; i++) {
+			for (key in arrayT[i]) {
+				let tr = document.createElement('tr');
+				let td = document.createElement('td');
+				td.innerHTML = key;
+				tr.appendChild(td);
+				for (let j = 0; j < arrayT[i][key].length; j++) {
+					let td = document.createElement('td');
+					td.innerHTML = arrayT[i][key][j];
+					tr.appendChild(td);
+				}
+				table.children[1].appendChild(tr);
+			}
+		}
 	})
 	.catch(error => {
 		console.error('Une erreur s\'est produite lors du chargement des données JSON :', error);
